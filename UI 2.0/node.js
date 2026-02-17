3158af8a40be01bca8467f62ab47fef32b21a77b
Nathan Bednar <144561616+bednarnatwit@users.noreply.github.com>
2026-02-17 10:15:01 -0500

    Delete UI 2.0 directory

--- UI 2.0/node.js
+++
@@ -1,9 +0,0 @@
-class GraphicalNode {
-    constructor(x, y, radius, label) {
-        this.x = x;
-        this.y = y;
-        this.radius = radius;
-        this.selected = false;
-        this.label = label;
-    }
-}
